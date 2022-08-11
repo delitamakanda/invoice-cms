2a622dbb3714e1c7c45339c0f8b66516885def81
Délita Makanda <delitamakanda@users.noreply.github.com>
2022-08-11 14:12:06 +0200

    Update wsgi.py

--- invoices/wsgi.py
+++ invoices/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invoices.settings')
+if settings.DEBUG:
+	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invoices.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invoices.settings_prod')
 
 application = get_wsgi_application()
