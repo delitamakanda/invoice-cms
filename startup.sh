2c828404d56a27144ddd63fd9a6cf6f1a4f6d36f
Délita Makanda <delitamakanda@users.noreply.github.com>
2022-08-11 16:58:59 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
-apt-get install wkhtmltopdf
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.stretch_amd64.deb
+apt-get install -y ./wkhtmltox_0.12.6-1.stretch_amd64.deb
+rm wkhtmltox_0.12.6-1.stretch_amd64.deb
 
 gunicorn --bind=0.0.0.0 --timeout 600 --chdir invoices wsgi  --access-logfile '-' --error-logfile '-'
