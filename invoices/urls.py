b0bdad5da3341541358a97d55004cef02712e281
Délita Makanda <delitamakanda@users.noreply.github.com>
2023-03-22 15:52:43 +0100

    Update urls.py

--- invoices/urls.py
+++ invoices/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
     path('admin/', admin.site.urls),
