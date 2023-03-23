8d13a61a15fc14b378a86b344e4c7cfb2cbd4eb6
Délita Makanda <delita.makanda@gmail.com>
2023-03-23 01:35:57 +0100

    fix(azure): fix sites framework

--- invoices/settings_prod.py
+++ invoices/settings_prod.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'apps.invoice.apps.InvoiceConfig',
 ]
 
+SITE_ID = 1
+
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
