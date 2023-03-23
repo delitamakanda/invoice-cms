05d59669c5c9c2ef6269af662a5cd5fa4951cb29
Délita Makanda <delita.makanda@gmail.com>
2023-03-23 01:28:30 +0100

    fix(azure): add package to prod

--- invoices/settings_prod.py
+++ invoices/settings_prod.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 
     'rest_framework',
     'rest_framework.authtoken',
