e113a4482958f2c95c11410dba8a3d295678da62
Délita Makanda <delita.makanda@gmail.com>
2023-10-08 21:07:45 +0200

    feat(datadog): add monitoring datadog

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,25 @@ import Antd from 'ant-design-vue'
 import filters from './helpers/filters'
 import { i18n } from './i18n/lang'
 import 'ant-design-vue/dist/antd.css'
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: process.env.DATADOG_APPLICATION_ID,
+    clientToken: process.env.DATADOG_CLIENT_TOKEN,
+    site: process.env.DATADOG_SITE,
+    service: process.env.DATADOG_SERVICE,
+    env: process.env.DATADOG_ENV,
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0', 
+    sessionSampleRate:100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
 
 axios.defaults.baseURL = '/'
 
