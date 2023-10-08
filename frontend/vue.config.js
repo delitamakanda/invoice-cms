b029808dfd73c8fbe1289d366e106b194485c82f
Délita Makanda <delita.makanda@gmail.com>
2023-10-08 23:07:10 +0200

    fix(datadog): fix env production

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 const BundleTacker = require('webpack-bundle-tracker')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/static/': 'http://0.0.0.0:8080',
   outputDir: './dist/',
+
+  configureWebpack: {
+    plugins: [
+      new NodePolyfillPlugin()
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
+    }
+  },
 
   chainWebpack: config => {
     config.optimization.splitChunks(false)
