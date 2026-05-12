a59906dbc814c2015325cc1c49b2527fa3477ea9
Délita Makanda <delita.makanda@gmail.com>
2026-05-12 22:10:23 +0200

    fix proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
       },
